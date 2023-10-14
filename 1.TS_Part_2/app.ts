ded911c9856f8f8cd01ce783960ca2761b34535c
Yaroslav Romaniuk <romaniuk007@gmail.com>
2023-10-15 03:47:33 +0400

    3.1 done

--- 1.TS_Part_2/app.ts
+++ 1.TS_Part_2/app.ts
@@ -86,7 +86,6 @@ function sumCValue(objectCalc: BigObject) : number {
   const keysArr: number[] = Object.keys(objectCalc).map((currentKey) : number => {
     const currentElement = objectCalc[currentKey];
 
-    console.log(currentElement)
 
     if (typeof currentElement === 'undefined')  return 2021;
     if (currentElement && typeof currentElement.cvalue === 'string') return parseInt(currentElement.cvalue) || 2021;
@@ -94,8 +93,6 @@ function sumCValue(objectCalc: BigObject) : number {
     if (currentElement.cvalue && isBigObject(currentElement.cvalue)) return sumCValue(currentElement.cvalue);
     return 0; // Added default return value
   });
-
-  console.log(keysArr)
 
   let sum:number = 0;
   for (let i = 0; i < keysArr.length; i++) {
@@ -114,27 +111,89 @@ function isBigObject(obj: any): obj is BigObject {
   );
 }
 
+console.log("CValue: " + sumCValue({ hello: {cvalue: 1}, world: { cvalue: { yay: { cvalue: "2" } } } }))
+
+// Напишите функцию mapObject, которая
+// в чем-то очень похожа на функцию map для массивов.
+
+// Эта функция должна принимать объект джаваскрипта
+// и функцию transformer, которую нужно применить к каждому из полей того объекта, 
+// ...а из результата применения функции transformer к каждому полю входящего объекта
+// собрать новый объект и вернуть его.
+
+// Так например можно будет замэппить объект типа 
+// { "roma" : 5, "vasya": 2 } оценок студентов
+// на функцию вроде (x) => x > 2
+// чтобы получить объект 
+// { "roma": true, "vasya": false } зачетов студентов
+
+// Понятное дело для описания сигнатуры mapObject надо будет юзать
+// 1) дженерики с несколькими параметрами-типами
+// 2) такую штуку как Record (globalThis.Record, если быть точным ;))
 
 
-//console.log(sumCValue({ undefined }));
+
+function mapObject<K extends string|number|symbol,T,Y>(myObject: Record<K,T>, transformer: (key:T) => Y): Record<K,Y>{
+  let result :Record<K,Y> = {} as Record<K,Y>;
+  for (let key in myObject){
+    result[key] = transformer(myObject[key])
+  }
+  return result;
+}
+
+
+console.log("mapObject: " + JSON.stringify(mapObject({ "roma" : 5, "vasya": 2 },(x) => x > 2)));
 
 
 
-//console.log(sumCValue({ hello: {cvalue: 1} }));
+// Напишите функцию, которая принимает:
+// 1) некие данные предполагаемо типа Т, но возможно не со всеми полями
+// 2) функцию-дополнятор, которая принимает такие штуки как из п.1, 
+//    а возвращает полноценный объект типа Т
+// ... как вы поняли, саму функцию писать не надо :) 
+// нас интересует только ее сигнатура.
 
-console.log(sumCValue({ hello: {cvalue: 1}, world: { cvalue: { yay: { cvalue: "2" } } } }));
+function anyFunction1<T>(data: Partial<T>, addData: (data: Partial<T>) => T){
+    return addData(data);
+}
 
-//console.log(isBigObject({ cvalue: { cvalue: 2 } }))
+// Более сложный вариант:
+// Напишите функцию, которая принимает:
+// 1) некие данные предполагаемо типа Т (у которого поле id: string), 
+//    но возможно без поля id
+// 2) функцию-дополнятор, которая принимает такие штуки как из п.1, 
+//    а возвращает полноценный объект типа Т
+// ... как вы поняли, саму функцию писать не надо :) 
+// нас интересует только ее сигнатура.
 
-//console.log(isBigObject({ hello: {cvalue: 1}, world: { cvalue: { yay: { cvalue: "2" } } } }))
+function anyFunction2<T extends {id?:string}>(data: Partial<T>, addData: (data: Partial<T>) => T){
+  return addData(data);
+}
 
 
-//let test = 1;
+// Последняя задача:
+// Напишите сигнатуру функции, которая принимает
+// - некий класс 
+// - количество
+// ...а возвращает массив экземпляров этого класса
 
+class Rectangle {
+  w!: number;
+  h!: number;
+}
+class Circle {
+  radius!: number;
+}
 
-//console.log (typeof test);
+// сделайте норм сигнатуру тут.
+// НЕТ, Rectangle|Circle это не вариант, надо сделать универсальную функцию 
+function наштамповать<T>(SOMECLASS: new () => T, count:number)  {
+  let a = []
+  for (let i = 0; i < count; i++)
+     a.push(new SOMECLASS());
+ 
+  return a;
+}
 
-
-//let test2 = '1'
-
-//console.log (typeof test2);
+let a: Rectangle[] = наштамповать(Rectangle, 10);
+let b: Circle[] = наштамповать(Circle, 20)
---
+++ 1.TS_Part_2/test.js
@@ -0,0 +1,7 @@
+let range = 5;
+
+range <= 3
+  ? console.log("мало")
+  : range >= 20 ?? range >= 4
+  ? console.log("марафонець!")
+  : console.log("красунчик!");
